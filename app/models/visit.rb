a96318da32ac617a8130233f00ee12ff5202b3e0
Varshaa <vsekarja@andrew.cmu.edu>
2022-03-28 21:48:37 -0400

    added model and controller

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,3 +1,20 @@
 class Visit < ApplicationRecord
-  belongs_to :patient
+  include AppHelpers::Deletions
+  
+  belongs_to :patient 
+
+  # Scopes
+  # by_admission -- orders results chronologically by admission date
+  # by_discharge -- orders results chronologically by discharge date
+  scope :by_admission, lambda { order('admission_date DESC') }
+  scope :by_discharge, lambda { order('discharge_date DESC') }
+
+  # Validations
+  validates_presence_of :patient_id,:account_number, :admission_date
+  validates_uniqueness_of :account_number
+  validates_date :admission_date, :on_or_before :discharge_date
+
+  before_destroy :cannot_destroy_object
+
+
 end
--- test/models/visit_test.rb
+++ test/models/visit_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class VisitTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to(:patient)
+  
+  
+  # Validation macros...
+  should validate_presence_of(:patient_id)
+  
+  # Validating presence...
+  should validate_presence_of(:account_number)
+  should validate_presence_of(:admission_date)
+  should validate_uniqueness_of(:account_number)
+
+  # Validating date...
+  should_not allow_value("bad").for(:admission_date)
+  should_not allow_value(2).for(:admission_date)
+  should_not allow_value(3.14159).for(:admission_date) 
+  should_not allow_value(nil).for(:admission_date) 
+  
+
 end
