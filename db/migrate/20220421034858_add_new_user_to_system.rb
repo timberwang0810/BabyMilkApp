eff4c333b1bd6d297393111216dc0edfccc7587d
Varshaa <vsekarja@andrew.cmu.edu>
2022-04-23 19:48:36 -0400

    authentication done with 2 users

--- db/migrate/20220421034858_add_new_user_to_system.rb
+++ db/migrate/20220421034858_add_new_user_to_system.rb
@@ -9,10 +9,20 @@ class AddNewUserToSystem < ActiveRecord::Migration[5.2]
       admin.password_confirmation = "secret"
       admin.role = "admin"
       admin.save!
+      nurse = User.new
+      nurse.first_name = "Nurse"
+      nurse.last_name = "Nurse"
+      nurse.username = "nurse@hahaha.com"
+      nurse.password = "secretNurse"
+      nurse.password_confirmation = "secretNurse"
+      nurse.role = "nurse"
+      nurse.save!
   end
   def down
       admin = User.find_by_username "admin@hahaha.com"
       User.delete admin
+      nurse = User.find_by_username "nurse@hahaha.com"
+      User.delete nurse
   end
 
   
