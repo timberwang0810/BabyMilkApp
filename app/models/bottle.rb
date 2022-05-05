e9896cdb7c3c5ca50495c3ad0188d203127c3ab6
Anagha Srikumar <asrikuma@andrew.cmu.edu>
2022-05-05 09:43:59 -0400

    fixed printer version

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -176,7 +176,7 @@ class Bottle < ApplicationRecord
         # File.open "./app/assets/images/qr/#{encrypted}.png", 'wb' do |f| # change file name for PNG images
         #     f.write rendered_zpl
         # end
-        print_job = Zebra::PrintJob.new 'Zebra_Technologies_ZTC_GX420t'
+        print_job = Zebra::PrintJob.new 'Zebra_Technologies_ZTC_GX420d'
         #print_job.print label, '192.168.1.63'
         print_job.print label, 'localhost'
     end
