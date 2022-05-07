fe1d1bd5b4dc1a517654d8ae6985efc67f406208
Anagha Srikumar <asrikuma@andrew.cmu.edu>
2022-05-07 18:27:15 -0400

    removing qr view

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -183,10 +183,10 @@ class Bottle < ApplicationRecord
     def generate_qr
         encrypted = encrypt(@@cipher_key, self.id.to_s)
         label = create_label
-        rendered_zpl = Labelary::Label.render zpl: print_zpl_str('raw_zpl', label)
-        File.open "./app/assets/images/qr/#{encrypted}.png", 'wb' do |f| # change file name for PNG images
-            f.write rendered_zpl
-        end
+        #rendered_zpl = Labelary::Label.render zpl: print_zpl_str('raw_zpl', label)
+        #File.open "./app/assets/images/qr/#{encrypted}.png", 'wb' do |f| # change file name for PNG images
+        #    f.write rendered_zpl
+        #end
         #print_job = Zebra::PrintJob.new 'Zebra_Technologies_ZTC_GX420d'
         #print_job.print label, '192.168.1.63'
         #print_job.print label, 'localhost'
